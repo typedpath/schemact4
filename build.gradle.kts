02681e5fc44c7ab7d63ba017850b0df5f419494a
roberttodd <roberttodd@typedpath.com>
2025-08-11 16:54:37 +0100

    upped versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.typedpath"
-version = "1.0.1"
+version = "1.0.2-SNAPSHOT"
 
 repositories {
     mavenCentral()
--- schemact-plugin/build.gradle.kts
+++ schemact-plugin/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.typedpath.schemact4"
-version = "1.0.1"
+version = "1.0.2-SNAPSHOT"
 
 repositories {
     mavenCentral()
